5eeeecda4b76bf004418df1fe4bd30e8aeb2666f
QUBITABHAY <codingcontestabhay@gmail.com>
2024-12-27 21:55:00 +0530

    Major Changes

--- basic_flask.py
+++ basic_flask.py
@@ -2,17 +2,35 @@ from flask import Flask
 from flask import render_template as e
 app = Flask(__name__)
 
+# Adding Dummy Data
+
+post = [
+    {
+        "author": "Abhay",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "December 27, 2024"
+    },
+    {
+        "author": "Nikhil",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "December 28, 2024"
+    }
+]
+
 # Making Home Page 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return e("home.html")
+    return e("home.html", post=post)
 
+# Making About Page
 
 @app.route("/about")
 def about():
-    return e("about.html")
+    return e("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
