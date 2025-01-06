fc7769700a0cf0ef1680e97f115b7ede35e8995e
QUBITABHAY <codingcontestabhay@gmail.com>
2025-01-07 00:37:34 +0530

    add profile updation feature

--- basic_flask/routes.py
+++ basic_flask/routes.py
@@ -1,8 +1,11 @@
+import secrets
+import os
 from flask import flash, redirect, render_template, url_for, request
 from basic_flask.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from basic_flask import app, db, bcrypt
 from basic_flask.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from bson import ObjectId
 
 # Dummy data remains the same for demonstration
 posts = [
@@ -148,20 +151,28 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
 
 @app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        # if form.picture.data:
-            # picture_file = save_picture(form.picture.data)
-            # current_user.image_file = picture_file
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.users.update_one(
-            {"_id": (current_user.id)},
-            {"$set": {"username": current_user.username, "email": current_user.email}}
+            {"_id": ObjectId(current_user.id)},
+            {"$set": {"username": current_user.username, "email": current_user.email, "image_file": current_user.image_file}}
         )
         flash("Your account has been updated!", "success")
         return redirect(url_for("account"))
